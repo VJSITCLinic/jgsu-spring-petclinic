0d4e7c24186234a1a9fb1aeec4aaa6a7f5b1a473
Vijay Tripuraneni <v_tripuraneni@hotmail.com>
2021-01-16 22:31:08 -0800

    fail test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("nofirstname");
+		person.setFirstName("notempty");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
